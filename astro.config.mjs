6648adecc65048c1515fb0433f1bcd228eb671ca
Guilherme <gui.rebello1@gmail.com>
2025-01-03 14:45:42 -0300

    Added oauth https://github.com/dorukgezici/astro-decap-cms-oauth

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,39 +3,39 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi'
 
+import decapCmsOauth from 'astro-decap-cms-oauth';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [
-				// Generate the OpenAPI documentation pages.
-				starlightOpenAPI([
-				  {
-				   base: 'api',
-				   label: 'Referências da API',
-				   schema: './src/schemas/apihub.json',
-				  },
-				]),
-			  ],
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guias',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Concepts',
-					autogenerate: { directory: 'reference' },
-				},
-				 // Add the generated sidebar group to the sidebar.
-				 ...openAPISidebarGroups,
-			],
-		}),
-	],
+    integrations: [starlight({
+        plugins: [
+            // Generate the OpenAPI documentation pages.
+            starlightOpenAPI([
+              {
+               base: 'api',
+               label: 'Referências da API',
+               schema: './src/schemas/apihub.json',
+              },
+            ]),
+          ],
+        title: 'My Docs',
+        social: {
+            github: 'https://github.com/withastro/starlight',
+        },
+        sidebar: [
+            {
+                label: 'Guias',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Concepts',
+                autogenerate: { directory: 'reference' },
+            },
+             // Add the generated sidebar group to the sidebar.
+             ...openAPISidebarGroups,
+        ],
+		}), decapCmsOauth()],
 });
